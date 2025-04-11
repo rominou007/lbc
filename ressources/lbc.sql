4d9a49d733aa1be6dc40288d8bfa1c2490f06e67
Antonnnn03 <120465246+Antonnnn03@users.noreply.github.com>
2025-04-11 11:41:27 +0200

    creation annonce
    ajout de la table annonce dans la base de données

--- ressources/lbc.sql
+++ ressources/lbc.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Apr 09, 2025 at 03:11 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Hôte : 127.0.0.1
+-- Généré le : ven. 11 avr. 2025 à 11:38
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,42 +18,67 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `lbc`
+-- Base de données : `lbc`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Structure de la table `annonce`
+--
+
+CREATE TABLE `annonce` (
+  `id` int(10) NOT NULL,
+  `titre` varchar(200) NOT NULL,
+  `photo` varchar(200) NOT NULL,
+  `prix` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
+  `ID` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
-  `mail` varchar(100) NOT NULL,
-  `phone` varchar(20) NOT NULL,
-  `mdp` varchar(255) NOT NULL,
-  `sexe` varchar(20) NOT NULL,
-  `access` int(2) NOT NULL DEFAULT 1
+  `Nom` varchar(50) NOT NULL,
+  `Prenom` varchar(50) NOT NULL,
+  `MAIL` varchar(255) NOT NULL,
+  `MDP` varchar(200) NOT NULL,
+  `Tel` varchar(20) NOT NULL,
+  `Access` int(11) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Indexes for dumped tables
+-- Index pour les tables déchargées
 --
 
 --
--- Indexes for table `users`
+-- Index pour la table `annonce`
+--
+ALTER TABLE `annonce`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`ID`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT pour la table `annonce`
+--
+ALTER TABLE `annonce`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
